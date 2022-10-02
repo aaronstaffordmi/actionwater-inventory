a9f3585767df07e935110f3e6b42a6f9aae5f110
Aaron Stafford <aaronstafford.co@gmail.com>
2022-10-01 20:59:32 -0400

    create inventory.php template in child theme

---
+++ app/public/wp-content/themes/salient-child/inventory.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ * Template Name: Inventory
+ * description: AWS Inventory 2022
+  Page template without sidebar
+ */
+
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+get_header();
+nectar_page_header( $post->ID );
+$nectar_fp_options = nectar_get_full_page_options();
+
+?>
+<div class="container-wrap">
+	<div class="<?php if ( $nectar_fp_options['page_full_screen_rows'] !== 'on' ) { echo 'container'; } ?> main-content" role="main">
+		<div class="row">
+			<?php
+
+			nectar_hook_before_content();
+
+			if ( have_posts() ) :
+				while ( have_posts() ) :
+
+					the_post();
+					the_content();
+
+				endwhile;
+			endif;
+
+			nectar_hook_after_content();
+
+			?>
+		</div>
+	</div>
+	<div class="green"><p>Hello World</p></div>
+	<?php nectar_hook_before_container_wrap_close(); ?>
+</div>
+<?php get_footer(); ?>
