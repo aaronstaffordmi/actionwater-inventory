066ad665ed73f94abf8ea345a03166ffad726a00
Aaron Stafford <aaronstafford.co@gmail.com>
2022-10-21 16:13:33 -0400

    create first part of listing card

--- app/public/wp-content/themes/salient-child/functions.php
+++ app/public/wp-content/themes/salient-child/functions.php
@@ -7,10 +7,16 @@ function salient_child_enqueue_styles() {
 		$nectar_theme_version = nectar_get_theme_version();
 		wp_enqueue_style( 'salient-child-style', get_stylesheet_directory_uri() . '/style.css', '', $nectar_theme_version );
 		
+		wp_enqueue_style( 'salient-child-inventory-style', get_stylesheet_directory_uri() . '/inventory-style.css', '', $nectar_theme_version );
+		
     if ( is_rtl() ) {
    		wp_enqueue_style(  'salient-rtl',  get_template_directory_uri(). '/rtl.css', array(), '1', 'screen' );
 		}
 }
 
+function stafford_js_scripts() {
+    wp_enqueue_script( 'custom-js', get_stylesheet_directory_uri() . '/staffordInventory.js', array( 'jquery' ),'',true );
+}
+add_action( 'wp_enqueue_scripts', 'stafford_js_scripts' );
 
 ?>
--- app/public/wp-content/themes/salient-child/inventory.php
+++ app/public/wp-content/themes/salient-child/inventory.php
@@ -18,6 +18,7 @@ $nectar_fp_options = nectar_get_full_page_options();
 ?>
 <div class="container-wrap">
 	<div class="<?php if ( $nectar_fp_options['page_full_screen_rows'] !== 'on' ) { echo 'container'; } ?> main-content" role="main">
+
 		<div class="row">
 			<?php
 
@@ -36,12 +37,90 @@ $nectar_fp_options = nectar_get_full_page_options();
 
 			?>
 		</div>
-	</div>
-	<?php nectar_hook_before_container_wrap_close(); ?>
-	<!-- Begin inventory template html -->
-	<div class="container">
-		<H2>Hello World</H2>
+<div class="container">
+
+	<!-- REFINE SECTION -->
+	<div class="app-container">
+		<div class="app-flex">
+			<div class="refine">
+				<div class="dropdown">
+					<h5 onclick="myFunction()">Location</h5>
+					<ul id="myDIV">
+					<li>
+						<input type="checkbox" value="hudsonville" id="hudsonville">
+						<label for="hudsonville">Hudsonville</label>
+					</li>
+					<li>
+						<input type="checkbox" value="fenton" id="fenton">
+						<label for="fenton">Fenton</label>
+					</li>
+					</ul>
+				</div>
+			</div>
+
+
+	<!-- CARD SECTION -->
+
+			<div class="card-outline">
+
+				<div class="boat-image">
+				</div>
+
+				<div class="boat-data">
+					<div class="left-column">
+						<div>
+							<h3>2023 NXT24</h3>
+							<div class="location">
+								<div class="location-icon"></div>
+								<h6>Hudsonville <span class="stock-num">#FH0348</span></h6>
+							</div>
+						</div>
+						<div class="stats">
+							<p>Length</p>
+						
+						</div>
+					</div>
+					<div class="right-column">
+						<div class="payment">
+						<div class="call">
+								<i style="font-size: 16px; line-height: 16px; height: 16px; width: 16px; color: #222222; margin: 5px 4px 0px 0px;" class="fa fa-phone"></i>
+								<h4>Call for Price</h4>
+							</div>
+						
+					
+						</div>
+						<div class="boat-ctas">
+							<div class="call">
+								<h3>$499<span class="month">/month*</span></h3>
+									
+								
+							</div>
+							<p class="terms">* +tax. 240months. 4.99% APR. $12,000 down payment.</p>
+							<a href="" id="contact-button">View Details</a>
+				
+							<a href="" id="details-button">View Details</a>
+						
+							
+						
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
 	</div>
 
 </div>
+
+
+
+
+<!-- Wordpress end of page -->
+</div>
+	<?php nectar_hook_before_container_wrap_close(); ?>
+</div>
+
+
+
+
 <?php get_footer(); ?>
